2d980c3109036173b225897711c2287acfbf253e
Tên của bạn <nq.nghia2507@gmail.com>
2026-07-30 16:09:48 +0700

    sửa dashC, tagC

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,15 +3,20 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Chapter;
+use App\Models\Comment;
+use App\Models\Story;
+use App\Models\User;
 
 class DashboardController extends Controller
 {
-    /**
-     * Handle the incoming request.
-     */
-    public function __invoke(Request $request)
+    public function __invoke()
     {
-        //
+        return view('admin.dashboard', [
+            'storyCount' => Story::count(),
+            'chapterCount' => Chapter::count(),
+            'userCount' => User::count(),
+            'commentCount' => Comment::count(),
+        ]);
     }
 }
